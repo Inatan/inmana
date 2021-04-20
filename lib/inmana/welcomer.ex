fc9e9b414ffd59604baf5d20628b0d9f9500d3c2
inatan <inatan_hertzog@hotmail.com>
2021-04-20 12:08:15 -0300

    First day class, leaning first steps of elixir

---
+++ lib/inmana/welcomer.ex
@@ -0,0 +1,34 @@
+defmodule Inmana.Welcomer do
+  # Receber um nome e uma idade do usuario
+  # se o usuário chamar banana e tiver idade 42,  ele recebe uma mensagem especial
+  # se o usuário for maior de idade, ele recebe uma mensagem normal
+  # se o usuário for menor de idada, retornamos um erro
+  # temos que tratar o nome do usuário para entradas erradas, como "BaNaNa", "  BaNaNa \n"
+
+  # iex -S mix
+
+  # h String.upcase => toda a documentação
+
+  # alias  Iamana.Welcomer => Welcomer
+
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
+
+    name
+    |> String.trim()
+    |> String.downcase()
+    |> evaluate(age)
+  end
+
+  defp evaluate("banana", 42) do
+    {:ok, "You are very special banana"}
+  end
+
+  defp evaluate(name, age) when age >= 18 do
+    {:ok, "Welcom #{name}"}
+  end
+
+  defp evaluate(name, _age) do
+    {:error, "You shall not pass #{name}"}
+  end
+end
