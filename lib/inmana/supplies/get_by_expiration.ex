438b5c73208e62bb15cc28ecfde9f5953e84e7fb
inatan <inatan_hertzog@hotmail.com>
2021-04-22 13:02:35 -0300

    Add Mail service to supply that will expire

---
+++ lib/inmana/supplies/get_by_expiration.ex
@@ -0,0 +1,20 @@
+defmodule Inmana.Supplies.GetByExpiration do
+  alias Inmana.{Restaurant, Supply, Repo}
+  import Ecto.Query
+
+  def call do
+    today = Date.utc_today()
+    beginning_of_week = Date.beginning_of_week(today)
+    end_of_week = Date.end_of_week(today)
+
+    query =
+      from supply in Supply,
+        where:
+          supply.expiration_date >= ^beginning_of_week and supply.expiration_date <= ^end_of_week,
+        preload: [:restaurant]
+
+    query
+    |> Repo.all()
+    |> Enum.group_by(fn %Supply{restaurant: %Restaurant{email: email}} -> email end)
+  end
+end
