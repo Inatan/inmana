9d6006e5772274c10bf855a6fb4dd0b6306d122d
inatan <inatan_hertzog@hotmail.com>
2021-04-29 22:02:59 -0300

    Test Supply Controller get

--- test/inmana_web/controllers/supplies_controller_test.exs
+++ test/inmana_web/controllers/supplies_controller_test.exs
@@ -44,4 +44,43 @@ defmodule InmanaWeb.SuppliesControllerTest do
       assert response == expected_response
     end
   end
+
+  describe "show/2" do
+    test "when uuid is valid, returns the supply", %{conn: conn} do
+      params = %{
+        id: "9ec5259f-9ea2-4f43-8387-c69ad848f4a3"
+      }
+
+      response =
+        conn
+        |> get(Routes.supply_path(conn, :show, params))
+        |> json_response(:created)
+
+      assert %{
+               "message" => "Supply created!",
+               "supply" => %{
+                 "responsible" => "inatan",
+                 "description" => "uva 2",
+                 "expiration_date" => ~D[2021-04-23],
+                 "restaurant_id" => "2a8bffd3-48a5-4b92-8fe5-1ecfaa4202d8",
+                 "id" => _id
+               }
+             } = response
+    end
+
+    test "when uuid is invalid,  returns error", %{conn: conn} do
+      params = %{
+        id: "9ec5259f-9ea2-4f43-8387-c69ad8Z8f4a3"
+      }
+
+      expected_response = %{"message" => %{"responsible" => ["can't be blank"]}}
+
+      response =
+        conn
+        |> get(Routes.supply_path(conn, :show, params))
+        |> json_response(:bad_request)
+
+      assert response == expected_response
+    end
+  end
 end
